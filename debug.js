a4b85569ec85b823c07e94b33dbe963be389e902
venturas23 <126189782+Venturas23@users.noreply.github.com>
2025-01-23 18:54:50 -0300

    Delete debug.js

--- debug.js
+++
@@ -1,7 +0,0 @@
-function logDebug(message) {
-    const debugDiv = document.getElementById('debugLog');
-    if (debugDiv) {
-        debugDiv.innerHTML += `<p>${message}</p>`;
-        debugDiv.scrollTop = debugDiv.scrollHeight; // Mantém o scroll no final
-    }
-}
